471a9e73faf85514c1a98b488545869ff9f3fc8d
Lara Potjewyd <lara@Laras-MacBook-Pro.local>
2018-08-03 14:17:26 -0600

    final commit

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -6,9 +6,12 @@ export default class TextContainer extends Component {
     this.state = {
       text: ''
     }
+    this.updateText = this.updateText.bind(this)
   }
 
-  updateText(e) {
+    
+
+  updateText = (e) => {
     this.setState({
       text: e.target.value
     })
@@ -18,7 +21,7 @@ export default class TextContainer extends Component {
     return (
       <div className="textContainer">
         <textarea 
-          // style={  }
+          style={{fontFamily: this.props.fontFamily, fontSize: this.props.fontSize, color: this.props.fontColor}}
           onChange={this.updateText}
           value={this.state.text} 
           placeholder='Start typing your thoughts here!'
